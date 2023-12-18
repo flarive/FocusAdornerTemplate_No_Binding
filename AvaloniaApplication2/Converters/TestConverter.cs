2dd9aa01cc67f7d1fd5aa988d1235b9e8b36dead
Eviral <flarive@hotmail.com>
2023-12-18 22:17:29 +0100

    Initial commit

---
+++ AvaloniaApplication2/Converters/TestConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace AvaloniaApplication2.Converters
+{
+    public sealed class TestConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return AvaloniaProperty.UnsetValue;
+        }
+    }
+}
